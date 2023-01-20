f76226f0e08cec8aa9c33efd04bd3942b0a8c000
Nuria <ngarciacobas@danielcastelao.org>
2023-01-20 10:35:17 +0100

    Añadido listener de calculo y modificado metodo de result if -> when y creado second activity

--- app/src/main/java/com/example/intenttarea/MainActivity.kt
+++ app/src/main/java/com/example/intenttarea/MainActivity.kt
@@ -6,8 +6,10 @@ import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +24,42 @@ class MainActivity : AppCompatActivity() {
         }
         val calculo: Button = findViewById(R.id.button2)
         calculo.setOnClickListener {
+            val intento1 = Intent(this, SecondActivity::class.java)
+            // Añade datos adicionales al Intent
+            intento1.putExtra("EXTRA_MESSAGE", 8)
+            intento1.putExtra("EXTRA_MESSAGE1", 7)
+            // Inicia la segunda actividad
+            startActivityForResult(intento1, REQUEST_OPERATION)
 
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         val imageView = findViewById<ImageView>(R.id.imageView)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            //Recoge la foto y la muestra por pantalla
-            val thumbnail = data?.extras!!.get("data") as Bitmap
-            imageView.setImageBitmap(thumbnail)
-            // Do other work with full size photo saved in locationForPhotos
+        // Definimos el text view para mostrar el dato que nos manda la Second
+        val textView = findViewById<TextView>(R.id.texto)
+        if (resultCode != Activity.RESULT_OK) return
+        Log.d("asd", requestCode.toString())
+        // Puedo distinguir diferentes acciones según el requestCode
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> {
+                //Recoge la foto y la muestra por pantalla
+                val thumbnail = data?.extras!!.get("data") as Bitmap
+                imageView.setImageBitmap(thumbnail)
+                // Do other work with full size photo saved in locationForPhotos
+            }
+            REQUEST_OPERATION -> {
+                // si el intent no es null muestro el resultado
+                if (data != null) {
+                    Log.d("asd", "recibido")
+                    textView.text = data.getIntExtra("suma",0).toString()
+                }; }
         }
     }
 
     companion object {
         const val REQUEST_IMAGE_CAPTURE = 1
+        const val REQUEST_OPERATION = 2
     }
 }
