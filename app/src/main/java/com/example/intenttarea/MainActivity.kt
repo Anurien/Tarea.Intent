e4df48772f2f1bfb9f46f656e7dbc6693d2455c9
Nuria <ngarciacobas@danielcastelao.org>
2023-01-20 10:21:59 +0100

    Añadidos interfaz de botones y botones al main

--- app/src/main/java/com/example/intenttarea/MainActivity.kt
+++ app/src/main/java/com/example/intenttarea/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.intenttarea
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val foto: Button = findViewById(R.id.button)
+        foto.setOnClickListener {
+
+        }
+        val calculo: Button = findViewById(R.id.button2)
+        calculo.setOnClickListener {
+
+        }
+        
     }
 }
